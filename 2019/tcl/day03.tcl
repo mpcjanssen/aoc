f7f968d6bf0ca55aa68b1af06dd62fb919323f2d
Mark Janssen <mpc.janssen@gmail.com>
2020-03-07 10:50:34 +0100

    Add visualisation of lines

--- 2019/tcl/day03.tcl
+++ 2019/tcl/day03.tcl
@@ -64,6 +64,65 @@ proc part2 {} {
 }
 
 
+proc addvisualsegment {start desc} {
+     set dir [string index $desc 0]
+     set length [string range $desc 1 end]
+     lassign $start sx sy
+     switch -exact $dir {
+         U { set ey [expr {$sy+$length}] ; set ex $sx}
+         D { set ey [expr {$sy-$length}] ; set ex $sx}
+         R { set ex [expr {$sx+$length}] ; set ey $sy}
+         L { set ex [expr {$sx-$length}] ; set ey $sy}
+     }
+     return [list $ex $ey]            
+}
+
+proc visualizeline {c input color name} {
+    set current {0 0} 
+    set points $current
+    foreach desc [split $input , ] {
+        set current [addvisualsegment $current $desc]
+        lassign $current x y
+        if {$::minx > $x} {set ::minx $x}
+        if {$::miny > $y} {set ::miny $y}
+        if {$::maxx < $x} {set ::maxx $x}
+        if {$::maxy < $y} {set ::maxy $y}
+        lappend points {*}$current
+    }
+    $c create line {*}$points -fill $color
+}
+
+proc visualize {} {
+    set ::minx 0
+    set ::miny 0
+    set ::maxx 0
+    set ::maxy 0
+    
+    package require Tk
+    destroy .c
+    canvas .c
+
+    lassign [split $::data \n] in1 in2
+    visualizeline .c $in1 red in1
+    visualizeline .c $in2 green in2
+
+    set s [expr {abs(300.0/($::maxx-$::minx))}]
+    set s2 [expr {abs(300.0/($::maxy-$::miny))}]
+    puts $::minx
+    puts $::miny
+    puts $::maxx
+    puts $::maxy
+    if {$s2 < $s} {set s $s2}
+    set size [expr {5/$s}]
+    .c create oval -$size -$size $size $size -fill red
+    .c move all [expr {-$::minx}]  [expr {-$::miny}]
+    .c scale all 0 0 $s $s2
+
+    .c move all [expr {10}]  [expr {10}]
+    grid .c -sticky nsew 
+
+}
+
 
 
 
