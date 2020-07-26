375438a91c2a9f4875d137daebc590ea64dcc649
Mark Janssen <mpc.janssen@gmail.com>
2020-07-26 20:41:30 +0200

    Improved day 3

--- 2019/tcl/day03.tcl
+++ 2019/tcl/day03.tcl
@@ -6,65 +6,70 @@ package require intcode
 
 set data [read-input day03]
 
-proc line {input} {
+
+
+proc line {input name} {
+    set ::idx 0
     set line {{0 0}} 
+    upvar #0 $name dist
+    set dist(0,0) $::idx
+    incr ::idx
     foreach desc [split $input , ] {
-        addsegment line $desc
+        addsegment line $desc $name
     }
     return $line
 }
 
 
 
-proc addsegment {lineVar desc} {
+proc addsegment {lineVar desc name} {
      upvar $lineVar line
+    upvar #0 $name dist
      set dir [string index $desc 0]
      set length [string range $desc 1 end]
      set start [lindex $line end]
      lassign $start sx sy
+     set dx 0
+     set dy 0
      switch -exact $dir {
-         U {
-             foreach _ [range 1 $length] {
-                 incr sy
-                 lappend line [list $sx $sy]
-             }
-         }
-         D {
-             foreach _ [range 1 $length] {
-                 incr sy -1
-                 lappend line [list $sx $sy]
-             }
-         }
-        R {
-             foreach _ [range 1 $length] {
-                 incr sx
-                 lappend line [list $sx $sy]
-             }
-         }
-        L {
-             foreach _ [range 1 $length] {
-                 incr sx -1
-                 lappend line [list $sx $sy]
-             }
-         }
+         U { set dy 1}
+         D {set dy -1}
+         L {set dx -1}    
+         R {set dx 1}
      }
+
+    foreach _ [range 1 $length] {
+        incr sy $dy
+        incr sx $dx
+        set dist($sx,$sy) $::idx
+        incr ::idx
+        lappend line [list $sx $sy]
+    }
 }
 
+lassign [split $::data \n] in1 in2
+set l1 [line $in1 d1] 
+set l2 [line $in2 d2] 
 
+
+set ints [lintersect $l1 $l2]
 
 proc part1 {} {
-    lassign [split $::data \n] in1 in2
-    return [lindex [lsort -integer -index 0 [lmap x [lintersect [line $in2] [line $in1]] {list [manhattan $x] $x}]] 1]
+    
+    return [lindex [lsort -integer -index 0 [lmap x $::ints  {list [manhattan $x] $x}]] 1]
 }
 
-proc linesum {p l1 l2} {
-    sum [list [lsearch -exact $l1 $p]  [lsearch -exact $l2 $p]]
+proc linesum {p} {
+    sum [list [lsearch -exact $::l1 $p]  [lsearch -exact $::l2 $p]]
+}
+
+proc linesum2 {p} {
+    lassign $p x y
+    return [expr {$::d1($x,$y) + $::d2($x,$y)}]
 }
 
 proc part2 {} {
-    lassign [split $::data \n] in1 in2
-    set l1 [line $in1] ; set l2 [line $in2] ; set ints [lintersect $l1 $l2]
-    return [lindex [lsort -integer -index 0 [lmap x $ints {list [linesum $x $l1 $l2] $x}]] 1]
+    return [lindex [lsort -integer -index 0 [lmap x $::ints {list [linesum2 $x] $x}]] 1]
 }
 
 
