19fb780b717d90d3a9d6dfa77a8367476cd16b82
Mark Janssen <mpc.janssen@gmail.com>
2020-07-28 11:43:01 +0200

    Add day 13

--- 2019/tcl/day13.tcl
+++ 2019/tcl/day13.tcl
@@ -21,9 +21,39 @@ proc part1 {} {
 }
 
 proc part2 {} {
-    set res {}
-    foreach perm [permutations [range 5 9]] {
-        lappend res [dosimul $perm]
-    }
-    return [lindex [lsort -decreasing -integer $res ] 0]
+	set m [CintCode $::program]
+	$m setmem 0 2
+	set score 0
+	while {[$m state] ne "stopped"} {
+		$m run
+		# puts [$m state]
+		set outputs [$m outputs]
+		set paddle {}
+		foreach {x y t} $outputs {
+			if {$t == 4 && $x != -1} {
+				set ballx $x
+				set bally $y
+			}
+			if {$t == 3 && $x != -1} {
+				set paddlex  $x 
+				set paddley $y
+			}
+			if {$x == -1} {
+				set score $t
+			}
+		}
+		if {$ballx > $paddlex} {
+			$m input 1
+		} elseif {$ballx == $paddlex} {
+			$m input 0
+		} else {
+			$m input -1
+		}
+
+		# puts "Ball $ballx , $bally"
+		# puts "Paddel $paddlex, $paddley"
+		$m clearoutputs
+		# gets stdin
+	}
+	return $score
 }
