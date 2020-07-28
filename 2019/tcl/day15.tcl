48153efb618db4f365300740d2c071b359841ff8
Mark Janssen <mpc.janssen@gmail.com>
2020-07-28 19:58:11 +0200

    Add day15

--- 2019/tcl/day15.tcl
+++ 2019/tcl/day15.tcl
@@ -10,12 +10,14 @@ set program [split [read-input day15] ,]
 # puts $program
 
 
-package require Tk
-wm geometry . 900x900
-canvas .c
-pack .c -fill both -expand 1
 
-proc discover {bots worldVar} {
+
+set gen 0
+set dist 0
+# Discover the maze by cloning the robots and
+# spreading out to cover the whole field
+proc discover {bots worldVar visualize} {
+	incr ::gen
 	if {[llength $bots] == 0} return
 	upvar $worldVar world
 	set nextgen {}
@@ -50,49 +52,105 @@ proc discover {bots worldVar} {
 		set cx [expr {$x*10 + 300}]
 		set cy [expr {$y*10 + 300}]
 		switch $out {
-			0 { 
-				puts wall
-				square .c $cx $cy 10 red wall
+			0 {
+				if {$visualize} { 
+					square .c $cx $cy 10 red wall
 					update
+				}
 				set world($loc) X
 				rename $newbot {}
 			}
 			1 {
 				set world($loc) .
-				square .c $cx $cy 10 white path
+				if {$visualize} {
+					square .c $cx $cy 10 white [list open $loc]
 					update
+				}
+				set ::maxgen [expr {$::gen  + 1}]
 				lappend nextgen $newbot $loc
 			   }
 			2 {
 				set world($loc) @
+				if {$visualize} {
+					square .c $cx $cy 10 blue oxy
 					update
-				square .c $cx $cy 10 blue oxy
+				}
+				set ::oxygen $::gen
 				lappend nextgen $newbot $loc
 			}
 		}
 	}
 	rename $bot {}
 	}
-
-	puts xxx[llength $nextgen]
-	puts [llength [array names world]]
-
-
-	tailcall discover $nextgen world
+	tailcall discover $nextgen world $visualize
 }
 
-proc part1 {} {
+proc part1 {{visualize 0}} {
+	upvar w world
 	unset -nocomplain world
 	set world(0,0) .
-	square .c 300 300 10 green start
+	if {$visualize} {
+		package require Tk
+		wm geometry . 900x900
+		canvas .c
+		label .l1 -textvariable  ::gen -anchor w
+		label .l2 -textvariable  ::dist -anchor w
+		grid .l1 -sticky ew
+		grid .l2 -sticky ew
+		grid .c -sticky nsew 
+		grid columnconfigure . .c -weight 1
+		grid rowconfigure . .c -weight 1
+		square .c 300 300 10 green {open 0,0}
+	}
 	set bot [CintCode $::program]
-	discover [list $bot 0,0] world
+	discover [list $bot 0,0] world $visualize
+	return $::oxygen
 	
+}
+
+proc distance {loc1 loc2} {
+	lassign [split $loc1 ,] x1 y1
+	lassign [split $loc2 ,] x2 y2
+	return [expr {abs($x1-$x2)+abs($y1-$y2)}]
+}
+
+proc part2 {{visualize 0}} {
+	upvar w world
+	foreach loc [array names world] {
+		switch -exact $world($loc) {
+			. {set tofill($loc) 1}
+			@ {set filled $loc}
+		}
+	}
+	while {1} {
+		if {[llength [array names tofill]] == 0} {
+			return $::dist
+		}
+		incr ::dist
+		set newfilled {}
+		foreach p1 $filled {
+			foreach p2 [array names tofill] {
+				if {[distance $p1 $p2]==1} {
+					if {$visualize} {
+						.c itemconfigure $p2 -fill lightblue
+						update
+						after 5
+					}
+					lappend newfilled $p2
+					unset tofill($p2) 
+				}
+			}
+		
+		}
+		set filled $newfilled
+		# puts [llength [array names tofill]]
+	}
 }
 
 
 
 if {$::argv0 eq [info script]} {
-	part1
-	puts [info commands cintcode::*]
+	part1 1
+	part2 1
+	catch {console show}
 }
